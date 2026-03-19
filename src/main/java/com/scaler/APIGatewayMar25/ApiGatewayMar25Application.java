28436205af5ecb51c5e0b8e5ce33bc9ec0ea77bd
Jay Yadav <work7028@gmail.com>
2026-03-19 15:56:58 +0530

    Initial commit - API Gateway setup

---
+++ src/main/java/com/scaler/APIGatewayMar25/ApiGatewayMar25Application.java
@@ -0,0 +1,13 @@
+package com.scaler.APIGatewayMar25;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiGatewayMar25Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGatewayMar25Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/scaler/APIGatewayMar25/ApiGatewayMar25ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.scaler.APIGatewayMar25;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayMar25ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
